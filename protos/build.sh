d99443831db0a86f1268127664327c690595ab6b
alb-luffy <zhaohb.cz@gmail.com>
2023-06-08 01:19:26 -0700

    add protos used for enclave communication

--- protos/build.sh
+++ protos/build.sh
@@ -6,3 +6,14 @@
 
 /usr/local/bin/protoc --proto_path=. --grpc_out=./untrusted/ --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin Regulator.proto
 /usr/local/bin/protoc --proto_path=. --grpc_out=./untrusted/ --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin Register.proto
+
+
+# trusted protobuf generate instruction
+/home/hongbo/protobuf/bin/protoc --proto_path=. --cpp_out=./trusted/binding_enclave Register.proto
+/home/hongbo/protobuf/bin/protoc --proto_path=. --cpp_out=./trusted/binding_enclave Regulator.proto
+/home/hongbo/protobuf/bin/protoc --proto_path=. --cpp_out=./trusted/binding_enclave BindingQuery.proto
+
+/home/hongbo/protobuf/bin/protoc --proto_path=. --cpp_out=./trusted/e2e_message E2EMessage.proto
+/home/hongbo/protobuf/bin/protoc --proto_path=. --cpp_out=./trusted/e2e_message RuleCheck.proto
+/home/hongbo/protobuf/bin/protoc --proto_path=. --cpp_out=./trusted/e2e_message Signature.proto
+/home/hongbo/protobuf/bin/protoc --proto_path=. --cpp_out=./trusted/rule_enclave EntityQuery.proto
